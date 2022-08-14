660753d607d586f98a79a1aa0bbb61c085a250dc
Jooyong Park <parkjy3321@inha.edu>
2022-08-14 23:43:06 +0900

    gitignore test

--- build/ea_ipm/CMakeFiles/ea_ipm_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ea_ipm_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ea_ipm_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
